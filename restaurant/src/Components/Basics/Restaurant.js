4e8d72f774fd5a002eb7a440c2bc02124637c883
Gaurav Gunjal <gaurav.gunjal@ic.gumgum.com>
2022-06-30 10:46:47 +0530

    displaying data beautifully

--- restaurant/src/Components/Basics/Restaurant.js
+++ restaurant/src/Components/Basics/Restaurant.js
@@ -1,25 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import Menu from "./menuApi";
+import MenuCard from "./MenuCard";
 
 const Restaurant = () => {
+    const [menuData, setMenuData] = useState(Menu);
+
     return (
         <>
-            <div className="card-container">
-                <div className="card">
-                    <div className="card-body">
-                        <span className="card-number card-circle subtle">1</span>
-                        <span className="card-author subtle">Breakfast</span>
-                        <h2 className="card-title"> Maggi </h2>
-                        <span className="card-description subtle">
-                            I love Maggi realy oo yues Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta
-                            ipsam autem eius necessitatibus fugiat in .
-                        </span>
-                        <div className="card-read">Read</div>
-                    </div>
-                    <img src={image} alt="images" className="card-media" />
-                    <span className="card-tag_subtle">Order Now</span>
-                </div>
-            </div>
+            <MenuCard menuData={menuData} />
         </>
     );
 };

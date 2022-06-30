2d3803540506b8397c456053ff2f4a8a81074bc2
Gaurav Gunjal <gaurav.gunjal@ic.gumgum.com>
2022-06-30 10:53:29 +0530

    navbar added

---
+++ restaurant/src/Components/Basics/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="btn-group">
+                    <button className="btn-group__item">Breakfast</button>
+                    <button className="btn-group__item">Lunch</button>
+                    <button className="btn-group__item">Evening</button>
+                    <button className="btn-group__item">Dinner</button>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
--- restaurant/src/Components/Basics/Restaurant.js
+++ restaurant/src/Components/Basics/Restaurant.js
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 import "./styles.css";
 import Menu from "./menuApi";
 import MenuCard from "./MenuCard";
+import Navbar from "./Navbar";
 
 const Restaurant = () => {
     const [menuData, setMenuData] = useState(Menu);
 
     return (
         <>
+			<Navbar/>
+
             <MenuCard menuData={menuData} />
         </>
     );
